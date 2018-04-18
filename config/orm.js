2b6027717b6db5a8e3cf1ff4d72c434a58b86f48
unknown <tberry6@gmail.com>
2018-04-18 12:52:42 -0700

    Wrote more of orm, now including callbacks

--- config/orm.js
+++ config/orm.js
@@ -1,20 +1,22 @@
 var connection = require("./connection.js");
 
 var orm = {
-    selectAll: function() {
+    selectAll: function(callback) {
         connection.query("SELECT * FROM burgers", function(error, results, fields) {
             if(error) throw error;
-            
+            callback(results);
         });
     },
-    insertOne: function(name) {
-        connection.query("INSERT INTO burgers SET ?", {burger_name: name, devoured: false}, function(error) {
+    insertOne: function(name, callback) {
+        connection.query("INSERT INTO burgers SET ?", {burger_name: name, devoured: false}, function(error, results, fields) {
             if(error) throw error;
+            callback(results);
         });
     },
-    updateOne: function(name) {
-        connection.query("UPDATE burgers SET ? WHERE burger_name = ?", [{devoured: true}, name], function(error) {
+    updateOne: function(name, callback) {
+        connection.query("UPDATE burgers SET ? WHERE burger_name = ?", [{devoured: true}, name], function(error, results, fields) {
             if(error) throw error;
+            callback(results);
         })
     }
 };
--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var app = express();
+var PORT = process.env.PORT || 7000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
