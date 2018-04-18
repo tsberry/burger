a8444ee08dfb67e438cf61ac2c0099a0219e1247
unknown <tberry6@gmail.com>
2018-04-18 12:25:27 -0700

    Added to orm

--- config/orm.js
+++ config/orm.js
@@ -2,13 +2,20 @@ var connection = require("./connection.js");
 
 var orm = {
     selectAll: function() {
-
+        connection.query("SELECT * FROM burgers", function(error, results, fields) {
+            if(error) throw error;
+            
+        });
     },
-    insertOne: function() {
-
+    insertOne: function(name) {
+        connection.query("INSERT INTO burgers SET ?", {burger_name: name, devoured: false}, function(error) {
+            if(error) throw error;
+        });
     },
-    updateOne: function() {
-        
+    updateOne: function(name) {
+        connection.query("UPDATE burgers SET ? WHERE burger_name = ?", [{devoured: true}, name], function(error) {
+            if(error) throw error;
+        })
     }
 };
 
